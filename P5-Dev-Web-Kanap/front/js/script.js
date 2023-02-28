09179b60bc8c6e1701e053f2fec9ad51c28ed979
Axel75018 <axel.calvet@gmail.com>
2023-02-28 10:20:00 +0100

    page accueil ok

--- P5-Dev-Web-Kanap/front/js/script.js
+++ P5-Dev-Web-Kanap/front/js/script.js
@@ -2,25 +2,26 @@ const urlAPI = "http://localhost:3000/api/products/"
 
 //Youtube
 console.log('youtube promesse');
-
-
-
+/*
 await fetch(urlAPI).then(response => response.json().then(data => console.log(data)));
 await fetch(urlAPI).then(response => response.json().then(data => console.log(data[1])));
 await fetch(urlAPI).then(response => response.json().then(data => console.log(data[1].name)));
-await fetch('http://localhost:3000/api/products/77711f0e466b4ddf953f677d30b0efc9').then(response => response.json().then(data => console.log(data)));
+await fetch('http://localhost:3000/api/products/77711f0e466b4ddf953f677d30b0efc9').then(response => response.json().then(data => console.log(data)));*/
 
 // Openclass room
 
 console.log('------------openclassroom await-----------------');
+//
+
 const reponse = await fetch(urlAPI);
+//transforme en json exploitable
 const reponseJSON = await reponse.json();
-console.log(reponseJSON);
+/*console.log(reponseJSON);
 console.log(reponseJSON[1]);
 console.log(reponseJSON[1].name);
 const reponseID = await fetch(`${urlAPI}77711f0e466b4ddf953f677d30b0efc9`);
 const reponseIDJSON = await reponseID.json();
-console.log(reponseIDJSON);
+console.log(reponseIDJSON);*/
 
 // Récupération de l'élément du DOM qui accueillera les fiches
 const sectionItems = document.querySelector(".items");
@@ -40,7 +41,7 @@ for (let i=0 ; i < reponseJSON.length ; i++) {
     produitNom.className = "productName";
     const produitDescription = document.createElement("p");
     produitDescription.innerText =  reponseJSON[i].description;
-    produitDescription.className = "productDescription"
+    produitDescription.className = "productDescription";
     
     // attache au dom
     sectionItems.appendChild(produitLien);
