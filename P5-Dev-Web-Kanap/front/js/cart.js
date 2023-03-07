cc3356906bf7929714d4773a8ff41703a09b8fac
Axel75018 <axel.calvet@gmail.com>
2023-03-07 09:18:30 +0100

    Probleme trouvé  valeur au click

--- P5-Dev-Web-Kanap/front/js/cart.js
+++ P5-Dev-Web-Kanap/front/js/cart.js
@@ -1,10 +1,12 @@
 console.log('cart loaded');
 
-//intitulé pour le local storage
-const cartID = "id";
-const cartCouleur = "couleur";
-const cartQuantite = "quantite";
-
+/*const id = 0;
+const couleur="couleur";
+const quantite="1";
+console.log(quantite);
+console.log(id);
+console.log(couleur);
+*/
 //localstorage:n
 
 //crétation fonction
@@ -12,13 +14,16 @@ const cartQuantite = "quantite";
 export function ajoutCart(id, couleur, quantite) {
 
 
+
+    localStorage.setItem("panier", JSON.stringify([id, couleur, quantite]));
+
     // controle  couleur présent et  q >0 <100
-    if (couleur == "" || quantite < 0 || quantite > 100) { alert('Séletionnez une couleur ET une quantité'); }
+    /*if (couleur == "" || quantite < 0 || quantite > 100) { alert('Séletionnez une couleur ET une quantité'); }
     else {
 
         // enrgistre dans locale storage panier
 
-        localStorage.setItem("panier", JSON.stringify({ 'cartID': id, 'couleur': couleur, 'Quantité': quantite }))
-    }
+        localStorage.setItem("panier", JSON.stringify([id, couleur, quantite]));
+    }*/
 
-}
+};
